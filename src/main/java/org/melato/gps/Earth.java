01b44a871f48bd6979b7a8ceb280da3c1b84e353
Alex Athanasopoulos <alex@melato.org>
2013-02-21 09:48:25 +0200

    updated copyright

--- src/main/java/org/melato/gps/Earth.java
+++ src/main/java/org/melato/gps/Earth.java
@@ -1,5 +1,5 @@
 /*-------------------------------------------------------------------------
- * Copyright (c) 2012, Alex Athanasopoulos.  All Rights Reserved.
+ * Copyright (c) 2012,2013, Alex Athanasopoulos.  All Rights Reserved.
  * alex@melato.org
  *-------------------------------------------------------------------------
  * This program is free software: you can redistribute it and/or modify
